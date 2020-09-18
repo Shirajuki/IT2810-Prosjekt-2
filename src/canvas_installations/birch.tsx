c3404a95274b38e0f9a038cf40f2e705062d7460
jonnynl <jonnynl@gitlab.stud.idi.ntnu.no>
2020-09-18 09:22:16 +0000

    Added more installations

---
+++ src/canvas_installations/birch.tsx
@@ -0,0 +1,291 @@
+import React, { useEffect, useRef, useState } from "react";
+import Poem from "../Poem";
+
+function Birch() {
+  const canvas = useRef<HTMLCanvasElement>(null);
+  const [ctx, setCtx] = useState(null);
+
+  useEffect(() => {
+    setCtx(canvas.current?.getContext("2d"));
+  }, [canvas]);
+  const drawPineTrees = (ctx: CanvasRenderingContext2D, x: number, y: number, sX?: number, sY?: number) => {
+    sX = sX || 1;
+    sY = sY || 1;
+		// Scaled rectangle
+		ctx.scale(sX, sY);
+		// Pine trees
+		ctx.beginPath();
+		ctx.fillStyle = "#3d913f";
+		// x = 59.5, y = 330
+		ctx.ellipse(x, y, 10, 25, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.beginPath();
+		ctx.fillStyle = "brown";
+		ctx.rect(x-7.5, y-40, 15, 40);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+			// Leaves
+		ctx.beginPath();
+		ctx.fillStyle = "#1a5234";
+		ctx.moveTo(x-39.5, y-20);
+		ctx.lineTo(x+43, y-20);
+		ctx.lineTo(x+1.75, y-55);
+		ctx.lineTo(x-39.5, y-20);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#183a23";
+		ctx.moveTo(x-31.5, y-40);
+		ctx.lineTo(x+35, y-40);
+		ctx.lineTo(x+1.75, y-75);
+		ctx.lineTo(x-31.5, y-40);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#1a5234";
+		ctx.moveTo(x-24.5, y-60);
+		ctx.lineTo(x+28, y-60);
+		ctx.lineTo(x+1.75, y-95);
+		ctx.lineTo(x-24.5, y-60);
+		ctx.fill();
+		ctx.closePath();
+		// Reset current transformation matrix to the identity matrix
+		ctx.setTransform(1, 0, 0, 1, 0, 0);
+	}
+  const draw = (ctx: CanvasRenderingContext2D) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.beginPath();
+    ctx.beginPath();
+		ctx.fillStyle = "#ffe0f8";
+		ctx.fillRect(0,0,750,450);
+		ctx.closePath();
+		// Background
+			// Mountain back
+		ctx.beginPath();
+		ctx.fillStyle = "#91d7ff";
+		ctx.moveTo(0, 120);
+		ctx.lineTo(40, 60);
+		ctx.lineTo(60, 100);
+		ctx.lineTo(90, 40);
+		ctx.lineTo(130, 90);
+		ctx.lineTo(170, 90);
+		ctx.lineTo(210, 120);
+		ctx.lineTo(250, 60);
+		ctx.lineTo(270, 50);
+		ctx.lineTo(300, 100);
+		ctx.lineTo(340, 20);
+		ctx.lineTo(400, 90);
+		ctx.lineTo(430, 100);
+		ctx.lineTo(470, 60);
+		ctx.lineTo(510, 85);
+		ctx.lineTo(530, 70);
+		ctx.lineTo(580, 110);
+		ctx.lineTo(610, 90);
+		ctx.lineTo(650, 110);
+		ctx.lineTo(680, 30);
+		ctx.lineTo(730, 90);
+		ctx.lineTo(740, 70);
+		ctx.lineTo(750, 80);
+		ctx.lineTo(750, 500);
+		ctx.lineTo(0, 500);
+		ctx.lineTo(0, 120);
+		ctx.fill();
+		ctx.closePath();
+			// Sun
+		ctx.beginPath();
+		ctx.fillStyle = "#fffcc9";
+		ctx.arc(180,130,110,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#fff68f";
+		ctx.arc(180,130,100,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#ffe959";
+		ctx.arc(180,130,90,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#ffd026";
+		ctx.arc(180,130,80,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+			// Mountain front
+		ctx.beginPath();
+		ctx.fillStyle = "#a8ffe5";
+		ctx.moveTo(0, 150);
+		ctx.lineTo(40, 100);
+		ctx.lineTo(60, 120);
+		ctx.lineTo(90, 120);
+		ctx.lineTo(130, 130);
+		ctx.lineTo(170, 120);
+		ctx.lineTo(210, 140);
+		ctx.lineTo(250, 120);
+		ctx.lineTo(270, 100);
+		ctx.lineTo(300, 140);
+		ctx.lineTo(340, 120);
+		ctx.lineTo(370, 140);
+		ctx.lineTo(400, 110);
+		ctx.lineTo(430, 130);
+		ctx.lineTo(470, 90);
+		ctx.lineTo(510, 110);
+		ctx.lineTo(530, 100);
+		ctx.lineTo(580, 140);
+		ctx.lineTo(610, 120);
+		ctx.lineTo(650, 130);
+		ctx.lineTo(690, 80);
+		ctx.lineTo(710, 110);
+		ctx.lineTo(730, 120);
+		ctx.lineTo(750, 100);
+		ctx.lineTo(750, 500);
+		ctx.lineTo(0, 500);
+		ctx.lineTo(0, 150);
+		ctx.fill();
+		ctx.closePath();
+	
+		// Grassland
+			// big circle
+		ctx.beginPath();
+		ctx.fillStyle = "#3c8c38";
+		ctx.arc(500,470,340,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#6bc257";
+		ctx.arc(500,470,330,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+			// small circle 
+		ctx.beginPath();
+		ctx.fillStyle = "#3c8c38";
+		ctx.arc(140,450,230,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#6bc257";
+		ctx.arc(140,450,220,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+	
+		// Birch tree
+			// Spot
+		ctx.beginPath();
+		ctx.fillStyle = "#3d913f";
+		ctx.ellipse(410, 230, 27, 55, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+			// Stump/Bark
+		ctx.beginPath();
+		ctx.fillStyle = "#f5e9e6";
+		ctx.lineWidth = 4;
+		ctx.moveTo(400, 230);
+		ctx.lineTo(385, 170);
+		ctx.lineTo(392, 158);
+		ctx.lineTo(375, 120);
+		ctx.lineTo(385, 120);
+		ctx.lineTo(398, 148);
+		ctx.lineTo(435, 90);
+		ctx.lineTo(445, 90);
+		ctx.lineTo(400, 172);
+		ctx.lineTo(420, 230);
+		ctx.lineTo(400, 230);
+		ctx.fill();
+		ctx.closePath();
+			// Person
+		ctx.beginPath();
+		ctx.fillStyle = "black";
+		ctx.rect(419,160,4,55);
+		ctx.rect(446,160,4,55);
+		ctx.rect(415,210,40,8);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "black";
+		ctx.arc(441,191,5,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "black";
+		ctx.ellipse(440, 203, 10, 4, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "black";
+		ctx.ellipse(436, 206, 8, 3, Math.PI/4, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "black";
+		ctx.ellipse(428, 205, 7, 3, -Math.PI/4, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "black";
+		ctx.ellipse(442, 203, 7, 5, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "black";
+		ctx.ellipse(435, 214, 6, 3, -Math.PI/2.5, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "black";
+		ctx.ellipse(435, 218, 6, 3, Math.PI/2.2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+			// Leaves
+		ctx.beginPath();
+		ctx.fillStyle = "#baff30";
+		ctx.ellipse(380, 115, 20, 25, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.beginPath();
+		ctx.fillStyle = "#e2ff26";
+		ctx.ellipse(365, 170, 24, 32, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.beginPath();
+		ctx.fillStyle = "#e2ff26";
+		ctx.ellipse(445, 100, 40, 55, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.beginPath();
+		ctx.fillStyle = "#baff30";
+		ctx.ellipse(435, 155, 20, 30, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		// Pine Trees
+		drawPineTrees(ctx,80,230,1.4,1.4);
+		drawPineTrees(ctx,60.5,330);
+		drawPineTrees(ctx,5,220,1.9,1.9);
+		drawPineTrees(ctx,60,300,1.6,1.6);
+			// Other side
+		drawPineTrees(ctx,465,265,1.3,1.3);
+		drawPineTrees(ctx,400,200,1.7,1.7);
+		drawPineTrees(ctx,320,210,2,2);
+		drawPineTrees(ctx,340,215,2.1,2.1);
+  };
+
+  useEffect(() => {
+    if (ctx) draw(ctx);
+  }, [ctx]);
+
+  return (
+    <>
+      <canvas ref={canvas} width="750" height="450" />
+      <Poem title={"The Birch-Tree at Loschwitz"}></Poem>
+    </>
+  );
+}
+
+export default Birch;
---
+++ src/canvas_installations/wallFlower.tsx
@@ -0,0 +1,286 @@
+import React, { useEffect, useRef, useState } from "react";
+import Poem from "../Poem";
+
+function wallFlower() {
+  const canvas = useRef<HTMLCanvasElement>(null);
+  const [ctx, setCtx] = useState(null);
+
+  useEffect(() => {
+    setCtx(canvas.current?.getContext("2d"));
+  }, [canvas]);
+  const drawPineTrees = (ctx: CanvasRenderingContext2D, x: number, y: number, rot?: number) => {
+	rot = rot || 0;
+	ctx.save()
+	ctx.translate(x + 750 / 2, y + 450 / 2);
+	ctx.rotate(rot);
+	ctx.beginPath();
+	ctx.fillStyle = "black";
+	ctx.fillRect(x,y,7,28);
+	ctx.closePath();
+	ctx.beginPath();
+	ctx.fillStyle = "black";
+	ctx.ellipse(x-1.5, y+29, 6, 8, Math.PI/2.5, 0, 2*Math.PI);
+	ctx.fill();
+	ctx.closePath();
+	ctx.beginPath();
+	ctx.fillStyle = "black";
+	ctx.fillRect(x,y,20,7);
+	ctx.closePath();
+	ctx.restore();
+	}
+  const draw = (ctx: CanvasRenderingContext2D) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.clearRect(0,0,750,450);
+		ctx.beginPath();
+		ctx.fillStyle = "#ffe0f8";
+		ctx.fillRect(0,0,750,450);
+		ctx.closePath();
+		// Background
+			// Wall
+		ctx.beginPath();
+		ctx.fillStyle = "#91d700";
+		ctx.moveTo(0, 250);
+		ctx.lineTo(500, 0);
+		ctx.lineTo(750, 0);
+		ctx.lineTo(750, 250);
+		ctx.lineTo(0, 250);
+		ctx.fill();
+		ctx.closePath();
+			// Floor
+		ctx.beginPath();
+		ctx.fillStyle = "#91d7ff";
+		ctx.fillRect(0,245,750,205);
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "pink";
+		ctx.moveTo(400, 450);
+		ctx.lineTo(750, 245);
+		ctx.lineTo(750, 450);
+		ctx.lineTo(400, 450);
+		ctx.fill();
+		ctx.closePath();
+		// Person 1
+		ctx.beginPath();
+		ctx.fillStyle = "deeppink";
+		ctx.arc(280,130,30,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "deeppink";
+		ctx.ellipse(280, 230, 90, 30, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#e51284";
+		ctx.arc(310,230,15,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		// Person 2
+		ctx.beginPath();
+		ctx.fillStyle = "deeppink";
+		ctx.arc(380,60,30,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "deeppink";
+		ctx.ellipse(380, 160, 90, 30, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#e51284";
+		ctx.arc(350,160,15,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#e51284";
+		ctx.arc(410,160,15,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		// Person 3
+		ctx.beginPath();
+		ctx.fillStyle = "#e51284";
+		ctx.arc(500,160,15,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "deeppink";
+		ctx.arc(530,70,30,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "deeppink";
+		ctx.ellipse(530, 170, 90, 30, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#e51284";
+		ctx.arc(550,170,15,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		// Person 4
+		ctx.beginPath();
+		ctx.fillStyle = "#e51284";
+		ctx.arc(415,230,15,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#e51284";
+		ctx.arc(465,230,15,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "deeppink";
+		ctx.arc(440,140,30,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "deeppink";
+		ctx.ellipse(440, 250, 90, 30, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		// Music box
+		ctx.beginPath();
+		ctx.fillStyle = "brown";
+		ctx.fillRect(40,220,130,100);
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "brown";
+		ctx.moveTo(40, 220);
+		ctx.lineTo(60, 180);
+		ctx.lineTo(80, 220);
+		ctx.lineTo(40, 220);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#82260d";
+		ctx.ellipse(110, 225, 17, 55, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "black";
+		ctx.ellipse(110, 225, 13, 50, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#e3b200";
+		ctx.moveTo(60, 180);
+		ctx.lineTo(170, 80);
+		ctx.lineTo(170, 190);
+		ctx.lineTo(57.5, 185);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#94680f";
+		ctx.ellipse(169, 136, 55, 13, Math.PI/2, 0, 2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		// Notes
+		drawNotes(ctx,-150,-110,-0.3);
+		drawNotes(ctx,-145,-90,0.1);
+		drawNotes(ctx,-95,-110,-0.5);
+		drawNotes(ctx,-110,-85,0.5);
+		// Disco
+		ctx.beginPath();
+		ctx.fillStyle = "rgba(0,0,255,0.2)";
+		ctx.arc(70,60,130,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "rgba(255,255,0,0.2)";
+		ctx.arc(20,250,90,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "rgba(0,255,255,0.2)";
+		ctx.arc(270,400,200,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "rgba(255,0,0,0.2)";
+		ctx.arc(600,50,250,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "rgba(0,255,0,0.2)";
+		ctx.arc(280,100,90,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "rgba(255,0,255,0.1)";
+		ctx.arc(500,300,60,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		// Flower
+		ctx.beginPath();
+		ctx.fillStyle = "#2f542b";
+		ctx.moveTo(650, 450);
+		ctx.lineTo(650, 360);
+		ctx.lineTo(630, 230);
+		ctx.lineTo(650, 230);
+		ctx.lineTo(670, 330);
+		ctx.lineTo(670, 450);
+		ctx.lineTo(650, 450);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#27695a";
+		ctx.moveTo(650, 420);
+		ctx.lineTo(610, 360);
+		ctx.lineTo(630, 360);
+		ctx.lineTo(650, 420);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#27695a";
+		ctx.moveTo(670, 400);
+		ctx.lineTo(720, 330);
+		ctx.lineTo(690, 345);
+		ctx.lineTo(670, 400);
+		ctx.fill();
+		ctx.closePath();
+			// Petal
+		ctx.beginPath();
+		ctx.fillStyle = "#a897a4";
+		ctx.arc(590,245,30,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#a897a4";
+		ctx.arc(617,289,35,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#a897a4";
+		ctx.arc(682,280,35,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#a897a4";
+		ctx.arc(682,220,40,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "#a897a4";
+		ctx.arc(630,210,40,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+		ctx.beginPath();
+		ctx.fillStyle = "orange";
+		ctx.arc(643,245,30,0,2*Math.PI);
+		ctx.fill();
+		ctx.closePath();
+  };
+
+  useEffect(() => {
+    if (ctx) draw(ctx);
+  }, [ctx]);
+
+  return (
+    <>
+      <canvas ref={canvas} width="750" height="450" />
+      <Poem title={"A Wall Flower"}></Poem>
+    </>
+  );
+}
+
+export default wallFlower;
