0825497feb7b093b144bac21f50fdd5b360705e5
Hanne K. Ødegård <hanne.1998@live.no>
2020-09-21 14:38:45 +0200

    Add theme stuff

--- src/App.tsx
+++ src/App.tsx
@@ -11,129 +11,150 @@ import Snow from "./canvas_installations/snow";
 import BlackForest from "./SVG_installations/BlackForest";
 import Birch from "./canvas_installations/birch";
 import WallFlower from "./canvas_installations/wallFlower";
+var background = "";
+var header = "";
+var hover = "";
+
+function setTheme(theme = "") {
+  if (theme === "black") {
+    background = "#171717";
+    header = "rgba(23, 23, 23, 0.9)";
+    hover = "#101010";
+  } else if (theme === "white") {
+    background = "#FFF";
+    header = "rgba(200, 200, 200, 0.9)";
+    hover = "#101010";
+  } else if (theme === "pink") {
+    background = "pink";
+    header = "darkpink";
+    hover = "#101010";
+  }
+  document.documentElement.style.setProperty("--background-colors", background);
+  document.documentElement.style.setProperty("--header-color", header);
+  document.documentElement.style.setProperty("--hover-color", hover);
+}
 
 function App() {
-	const [modal, setModal] = useState({
-		title: "",
-		picture: "",
-		sound: "",
-	});
-	return (
-		<>
-			<div className="wrapper">
-				<header>
-					<div>
-						<h1>
-							<a href="index.html" className="logo">
-								Kunst ustilling
-							</a>
-						</h1>
-						<nav>
-							<a href="#">nav1</a>
-							<a href="#">nav2</a>
-							<a href="#">nav3</a>
-							<a href="#">nav4</a>
-						</nav>
-					</div>
-				</header>
-				<br />
-				<main>
-					<Slideshow />
-					<div className="sectionsplit"></div>
-					<div className="sectionsplit"></div>
-					<div className="tabBox">
-						<a href="#">Tab 1</a>
-						<a href="#">Tab 2</a>
-					</div>
-					<div className="artWrapper">
-						<div className="art1">
-							<div>
-								<BlackForest
-									onClick={() =>
-										setModal({
-											title: "In the Black Forest",
-											picture: "./2.jpg",
-											sound: "Kalimba.mp3",
-										})
-									}
-								></BlackForest>
-							</div>
-							<div>
-								<div className="poetryBox">
-									<h2 className="titlefont">In The Black Forest</h2>
-									<Poem title={"In the Black Forest"}></Poem>
-								</div>
-							</div>
-						</div>
-						<div className="art2">
-							<div>
-								<Birch
-									onClick={() =>
-										setModal({
-											title: "The Birch-Tree at Loschwitz",
-											picture: "./2.jpg",
-											sound: "Kalimba.mp3",
-										})
-									}
-								></Birch>
-							</div>
-							<div>
-								<div className="poetryBox reversed">
-									<h2 className="titlefont">The Birch-Tree at Loschwitz</h2>
-									<Poem title={"The Birch-Tree at Loschwitz"}></Poem>
-								</div>
-							</div>
-						</div>
-						<div className="art3">
-							<div>
-								<WallFlower
-									onClick={() =>
-										setModal({
-											title: "A Wall Flower",
-											picture: "./2.jpg",
-											sound: "Kalimba.mp3",
-										})
-									}
-								></WallFlower>
-							</div>
-							<div>
-								<div className="poetryBox">
-									<h2 className="titlefont">A Wall Flower</h2>
-									<Poem title={"A Wall Flower"}></Poem>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div className="sectionsplit"></div>
-					<div id="root"></div>
-				</main>
-			</div>
-			<div className="sectionsplit2"></div>
-			<div
-				className="modal-container"
-				style={modal.title !== "" ? { display: "block" } : { display: "none" }}
-			>
-				<div id="modal-content">
-					<div
-						className="close-button"
-						id="myBtn"
-						onClick={() => setModal({ title: "", picture: "", sound: "" })}
-					>
-						<IoMdClose />
-					</div>
+  const [modal, setModal] = useState({
+    title: "",
+    picture: "",
+    sound: "",
+  });
+  return (
+    <>
+      <div className="wrapper">
+        <header>
+          <div>
+            <h1>
+              <a href="index.html" className="logo">
+                Kunst ustilling
+              </a>
+            </h1>
+            <nav>
+              <a onClick={() => setTheme("pink")}>Pink</a>
+              <a onClick={() => setTheme("white")}>Light</a>
+              <a onClick={() => setTheme("black")}>Dark</a>
+            </nav>
+          </div>
+        </header>
+        <br />
+        <main>
+          <Slideshow />
+          <div className="sectionsplit"></div>
+          <div className="sectionsplit"></div>
+          <div className="tabBox">
+            <a href="#">Tab 1</a>
+            <a href="#">Tab 2</a>
+          </div>
+          <div className="artWrapper">
+            <div className="art1">
+              <div>
+                <BlackForest
+                  onClick={() =>
+                    setModal({
+                      title: "In the Black Forest",
+                      picture: "./2.jpg",
+                      sound: "Kalimba.mp3",
+                    })
+                  }
+                ></BlackForest>
+              </div>
+              <div>
+                <div className="poetryBox">
+                  <h2 className="titlefont">In The Black Forest</h2>
+                  <Poem title={"In the Black Forest"}></Poem>
+                </div>
+              </div>
+            </div>
+            <div className="art2">
+              <div>
+                <Birch
+                  onClick={() =>
+                    setModal({
+                      title: "The Birch-Tree at Loschwitz",
+                      picture: "./2.jpg",
+                      sound: "Kalimba.mp3",
+                    })
+                  }
+                ></Birch>
+              </div>
+              <div>
+                <div className="poetryBox reversed">
+                  <h2 className="titlefont">The Birch-Tree at Loschwitz</h2>
+                  <Poem title={"The Birch-Tree at Loschwitz"}></Poem>
+                </div>
+              </div>
+            </div>
+            <div className="art3">
+              <div>
+                <WallFlower
+                  onClick={() =>
+                    setModal({
+                      title: "A Wall Flower",
+                      picture: "./2.jpg",
+                      sound: "Kalimba.mp3",
+                    })
+                  }
+                ></WallFlower>
+              </div>
+              <div>
+                <div className="poetryBox">
+                  <h2 className="titlefont">A Wall Flower</h2>
+                  <Poem title={"A Wall Flower"}></Poem>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="sectionsplit"></div>
+          <div id="root"></div>
+        </main>
+      </div>
+      <div className="sectionsplit2"></div>
+      <div
+        className="modal-container"
+        style={modal.title !== "" ? { display: "block" } : { display: "none" }}
+      >
+        <div id="modal-content">
+          <div
+            className="close-button"
+            id="myBtn"
+            onClick={() => setModal({ title: "", picture: "", sound: "" })}
+          >
+            <IoMdClose />
+          </div>
 
-					<Installation
-						title={modal.title}
-						picture={modal.picture}
-						sound={modal.sound}
-					></Installation>
-				</div>
-			</div>
+          <Installation
+            title={modal.title}
+            picture={modal.picture}
+            sound={modal.sound}
+          ></Installation>
+        </div>
+      </div>
 
-			<footer>
-				<p>this is a footer</p>
-			</footer>
-		</>
-	);
+      <footer>
+        <p>this is a footer</p>
+      </footer>
+    </>
+  );
 }
 export default App;
--- src/InstallationDisplay.tsx
+++ src/InstallationDisplay.tsx
@@ -6,30 +6,30 @@ import BlackForest from "./SVG_installations/BlackForest";
 import Birch from "./canvas_installations/birch";
 import WallFlower from "./canvas_installations/wallFlower";
 
-const Display = (props: {title: string, onClick?: () => void}) => {
-	const project = () => {
-		switch(props.title) {
-			case "A Wall Flower":
-				return <WallFlower onClick={props.onClick}></WallFlower>;
-			case "In the Black Forest":
-				return <BlackForest onClick={props.onClick}></BlackForest>;
-			case "The Birch-Tree at Loschwitz":
-				return <Birch onClick={props.onClick}></Birch>;
-			default:
-				return <h1>No project match</h1>
-		}
-	}
-	return (
-		project()
-	);
-}
+const Display = (props: { title: string; onClick?: () => void }) => {
+  const project = () => {
+    switch (props.title) {
+      case "A Wall Flower":
+        return <WallFlower onClick={props.onClick}></WallFlower>;
+      case "In the Black Forest":
+        return <BlackForest onClick={props.onClick}></BlackForest>;
+      case "The Birch-Tree at Loschwitz":
+        return <Birch onClick={props.onClick}></Birch>;
+      case "The Moon, how definite its orb! (fragment)":
+        return <Moon onClick={props.onClick}></Moon>;
+      default:
+        return <h1>No project match</h1>;
+    }
+  };
+  return project();
+};
 
-function InstallationDisplay( props: {title: string, onClick?: () => void} ) {
-	return (
-		<>
-			<Display title={props.title} onClick={props.onClick}></Display>
-		</>
-	);
+function InstallationDisplay(props: { title: string; onClick?: () => void }) {
+  return (
+    <>
+      <Display title={props.title} onClick={props.onClick}></Display>
+    </>
+  );
 }
 
 export default InstallationDisplay;
