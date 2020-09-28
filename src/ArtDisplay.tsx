d5c01d3ddfed7ac7be8169a2988f21cdfe30bc69
shirajuki <vulturegfd@reborn.com>
2020-09-28 11:23:37 +0200

    Added darkoverlay on installations

--- src/ArtDisplay.tsx
+++ src/ArtDisplay.tsx
@@ -5,23 +5,23 @@ import InstallationDisplay from "./InstallationDisplay";
 import Star from "./Star";
 
 function ArtDisplay(props: { title: string; setModal: () => void }) {
-  return (
-    <>
-      <div>
-        <InstallationDisplay
-          title={props.title}
-          onClick={() => props.setModal()}
-        ></InstallationDisplay>
-        <Star id={props.title} />
-      </div>
-      <div>
-        <div className="poetryBox reversed">
-          <h2 className="titlefont">{props.title}</h2>
-          <Poem title={props.title}></Poem>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<div className="InstallationWrapper">
+					<div className="darkOverlay" onClick={() => props.setModal()}>▶</div>
+					<InstallationDisplay title={props.title}></InstallationDisplay>
+					<Star id={props.title} />
+				</div>
+			</div>
+			<div>
+				<div className="poetryBox reversed">
+					<h2 className="titlefont">{props.title}</h2>
+					<Poem title={props.title}></Poem>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default ArtDisplay;
